dc10dabb1492d883b162381053cc7dd57cce22cf
andluiwinck <andluiwinck@hotmail.com>
2025-12-16 15:59:19 -0300

    added some print statements for debugging purposes

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,9 +2,14 @@ import os
 
 def get_files_info(working_directory, directory="."):
     try:
+        #print("Working dir:", working_directory)
+        #print("Directory:", directory)
         working_dir_abs = os.path.abspath(working_directory)
+        #print("Working dir abs:", working_dir_abs)
         target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+        #print("Target dir:", target_dir)
         valid_target_dir = os.path.commonpath([working_dir_abs, target_dir])
+        #print("Valid target dir:", valid_target_dir)
         if valid_target_dir != working_dir_abs:
             return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
         if not os.path.isdir(target_dir):
