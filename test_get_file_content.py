13c6661537bc0c62731ea6e8f8b9d1ee4eebfbae
andluiwinck <andluiwinck@hotmail.com>
2025-12-17 12:14:41 -0300

    added the functionality to write to files

--- test_get_file_content.py
+++ test_get_file_content.py
@@ -1,11 +1,16 @@
 from functions.get_file_content import get_file_content
 
-print(get_file_content("calculator", "lorem.txt"))
 
-print(get_file_content("calculator", "main.py"))
+def test():
+    result = get_file_content("calculator", "main.py")
+    print(result)
 
-print(get_file_content("calculator", "pkg/calculator.py"))
+    result = get_file_content("calculator", "pkg/calculator.py")
+    print(result)
 
-print(get_file_content("calculator", "/bin/cat"))
+    result = get_file_content("calculator", "/bin/cat")
+    print(result)
 
-print(get_file_content("calculator", "pkg/does_not_exist.py"))
+
+if __name__ == "__main__":
+    test()
