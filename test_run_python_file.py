9af31a913c1afba8c0e0ab17746179f2d224e0ac
andluiwinck <andluiwinck@hotmail.com>
2025-12-17 14:43:44 -0300

    added the functionality to run a Python file

---
+++ test_run_python_file.py
@@ -0,0 +1,31 @@
+from functions.run_python_file import run_python_file
+
+def test():
+    result = run_python_file("calculator", "main.py")
+    print("Result:")
+    print(result)
+    print("")
+
+    result = run_python_file("calculator", "main.py", ["3 + 5"])
+    print("Result:")
+    print(result)
+
+    result = run_python_file("calculator", "tests.py")
+    print("Result:")
+    print(result)
+
+    result = run_python_file("calculator", "../main.py")
+    print("Result:")
+    print(result)
+
+    result = run_python_file("calculator", "nonexistent.py")
+    print("Result:")
+    print(result)
+
+    result = run_python_file("calculator", "lorem.txt")
+    print("Result:")
+    print(result)
+
+
+if __name__ == "__main__":
+    test()
